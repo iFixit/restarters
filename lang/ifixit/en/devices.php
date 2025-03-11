7471a3acb6847f95681384a5a457583a29b9f271
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-03-11 13:34:23 -0700

    chore: add additional translation overrides

--- lang/ifixit/en/devices.php
+++ lang/ifixit/en/devices.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-'impact_read_more' => '<a target="_blank" href="https://www.ifixit.com/Right-to-Repair/E-waste">Read more</a> about how the gadgets we own have an impact on the environment. You can make our impact go even further.',
+    'impact_read_more' => '<a target="_blank" href="https://www.ifixit.com/Right-to-Repair/E-waste">Read more</a> about how the gadgets we own have an impact on the environment. You can make our impact go even further.',
 ];
