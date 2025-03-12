6696e572dacfcb879004cf570ebee995b370e63b
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-03-12 12:51:05 -0700

    feat(errors): add error reporting translation and update 404 page
    - Introduced a new translation for error reporting in errors.php
    - Updated the 404 error page to utilize the new translation for consistency
    - Use `{{!! !!}}` to not escape the content and allow HTML to be rendered

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -28,7 +28,7 @@
                 @endif
 
                 <p>
-                    You can report the issue by sending an email to <a href="mailto:community@therestartproject.org">community@therestartproject.org</a>, or by posting in the <a href="https://talk.restarters.net/c/help/17">restarters.net help forum</a>.
+                    {!! __('errors.report_issue') !!}
                 </p>
                 <p>
                     Please include the following details in your bug report:
