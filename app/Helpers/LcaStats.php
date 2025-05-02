8e7f1fc5dae193c83683216993803285aa623602
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-05-01 17:39:20 -0700

    fix(helpers): set default values for emission ratios and displacement factor
    Updated the LcaStats helper methods to include default values for emission ratios
    and displacement factor when environment variables are not set. This ensures
    consistent calculations and prevents potential errors due to missing environment
    variables.

--- app/Helpers/LcaStats.php
+++ app/Helpers/LcaStats.php
@@ -8,17 +8,17 @@ class LcaStats
 {
     public static function getEmissionRatioUnpowered()
     {
-        return env('EMISSION_RATIO_UNPOWERED');
+        return env('EMISSION_RATIO_UNPOWERED', 0.75);
     }
 
     public static function getEmissionRatioPowered()
     {
-        return env('EMISSION_RATIO_POWERED');
+        return env('EMISSION_RATIO_POWERED', 1.2);
     }
 
     public static function getDisplacementFactor()
     {
-        return env('DISPLACEMENT_VALUE');
+        return env('DISPLACEMENT_VALUE', 0.5);
     }
 
     /**
@@ -31,12 +31,16 @@ class LcaStats
         $eR = self::getEmissionRatioPowered();
         $uR = self::getEmissionRatioUnpowered();
 
+        $dF = is_numeric($dF) ? $dF : 0.5;
+        $eR = is_numeric($eR) ? $eR : 1.2;
+        $uR = is_numeric($uR) ? $uR : 0.75;
+
         $t1 = "
 SELECT
 CASE WHEN (c.powered = 1) THEN (CASE WHEN (c.weight = 0) THEN d.estimate ELSE c.weight END) ELSE 0 END  AS powered_device_weights,
 CASE WHEN (c.powered = 0) THEN (CASE WHEN (d.estimate = 0) THEN c.weight ELSE d.estimate END) ELSE 0 END AS unpowered_device_weights,
-CASE WHEN (c.powered = 1) THEN (CASE WHEN (c.weight = 0) THEN (d.estimate * $eR) ELSE c.footprint END) ELSE 0 END AS powered_footprints,
-CASE WHEN (c.powered = 0) THEN (CASE WHEN (d.estimate = 0) THEN c.footprint ELSE (d.estimate * $uR) END) ELSE 0 END AS unpowered_footprints
+CASE WHEN (c.powered = 1) THEN (CASE WHEN (c.weight = 0) THEN (d.estimate * {$eR}) ELSE c.footprint END) ELSE 0 END AS powered_footprints,
+CASE WHEN (c.powered = 0) THEN (CASE WHEN (d.estimate = 0) THEN c.footprint ELSE (d.estimate * {$uR}) END) ELSE 0 END AS unpowered_footprints
 FROM devices d, categories c, events e
 WHERE d.category = c.idcategories
 AND d.event = e.idevents
@@ -49,8 +53,8 @@ AND d.repair_status = 1
 SELECT
 SUM(t1.powered_device_weights) AS powered_waste,
 SUM(t1.unpowered_device_weights) AS unpowered_waste,
-SUM(t1.powered_footprints) * $dF AS powered_footprint,
-SUM(t1.unpowered_footprints) * $dF AS unpowered_footprint
+SUM(t1.powered_footprints) * {$dF} AS powered_footprint,
+SUM(t1.unpowered_footprints) * {$dF} AS unpowered_footprint
 FROM ($t1) t1
 ";
 
