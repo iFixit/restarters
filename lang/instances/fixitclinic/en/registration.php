c5cf5947812a94d8a4435786dc67b4b00516fc16
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-05-14 14:50:52 -0700

    refactor(translations): update registration consent text and add data usage link

--- lang/instances/fixitclinic/en/registration.php
+++ lang/instances/fixitclinic/en/registration.php
@@ -2,11 +2,12 @@
 
 return [
     'reg-step-1-heading' => 'What skills would you like to share with others on iFixit?',
+
     'reg-step-3-1a' => 'We can send you email updates about events and groups related to you, and about iFixit in general.',
     'reg-step-3-label1' => 'I would like to receive monthly newsletter',
     'reg-step-3-label2' => 'I would like to receive email notifications about events or groups near me',
+
     'reg-step-4-label1' => '<strong>Personal Data</strong>. I consent for iFixit to use my personal data internally for the purposes of registering me in the community, verifying the source of repair data and improving the volunteer experience. I understand that my personal profile will be visible to other community members, however personal data provided will never be shared with third parties without my consent. I understand I can delete my profile and all my personal data at any time - and access the community Privacy Policy <a href="https://www.ifixit.com/Info/Privacy" rel="noopener noreferrer" target="_blank">here</a>.',
-    'reg-step-4-label2' => '<strong>Repair Data</strong>.  By contributing repair data to the Fixometer, I give iFixit a perpetual royalty-free license to use it. The license allows iFixit to distribute the data under any open license and to retain any non-personal data even in case I request deletion of my personal profile on the Fixometer.)',
-    'reg-step-4-label3' => '<strong>Historical Repair Data</strong>. I give a perpetual royalty-free license to any of my previous repair data contributions to iFixit. The license allows     to distribute the data under any open license and to retain any non-personal data even in case I request deletion of my personal profile on the Fixometer.)',
+    'reg-step-4-label2' => '<strong>Repair Data</strong>.  By contributing repair data to the Fixometer, I give iFixit a perpetual royalty-free license to use it. The license allows iFixit to distribute the data under any open license and to retain any non-personal data even in case I request deletion of my personal profile on the Fixometer.) (<i>Read more about how we use repair data <a href="https://therestartproject.org/fixometer/why-we-collect-repair-data/" rel="noopener noreferrer" target="_blank">here</a></i>.)',
     'complete-profile' => 'Complete my profile',
 ];
