6c5dce287fdce3a6ad855a57985fab6d2d152518
Angel de la Torre <angel.dlt.1996@gmail.com>
2025-06-01 16:55:33 -0700

    fix(docker): update HTTPS middleware to allow local testing of production image
    We needed to update the HTTPS middleware. If we attempt to use the
    use the production image locally then we will get SSL errors because
    the app is configured to use HTTPS. As such, we will add a conditional
    check to see if both APP_ENV is production and APP_DEBUG is true. This
    scenario should only happen when we are testing the production image
    locally. If this condition is met then we will downgrade the request
    to use HTTP.

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -20,6 +20,15 @@ class HttpsProtocol
             return $next($request);
         }
 
+        // This is meant for local testing of the production image where APP_DEBUG can be set to true
+        if (env('APP_ENV') === 'production' && env('APP_DEBUG') === true) {
+            $host = $request->getHost();
+            if (in_array($host, ['localhost', '127.0.0.1', '::1']) || str_contains($host, '.local')) {
+                \URL::forceScheme('http');
+                return $next($request);
+            }
+        }
+
         if (! $request->secure() && (env('APP_ENV') === 'development' || env('APP_ENV') === 'production')) {
             // Force URL generation to use HTTPS
             \URL::forceScheme('https');
